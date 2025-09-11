4e599014f57f96b0fb69acf2dc81b7cfb6ab07c9
CesarAntolinez <cesaralejandroantolinez@gmail.com>
2025-09-10 22:20:01 -0500

    agregar test para etiquetas

---
+++ tests/Feature/TagTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Models\{Tag, User};
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\Response;
+use Laravel\Sanctum\Sanctum;
+
+pest()->use(RefreshDatabase::class);
+
+test('tag index', function () {
+    Sanctum::actingAs(User::factory()->create());
+
+    Tag::factory(3)->create();
+
+    $response = $this->getJson('/api/tags');
+
+    // $response->assertStatus(200);
+    $response->assertJsonCount(3, 'data')
+        ->assertJsonStructure([
+            'data' => [
+                [
+                    'id',
+                    'type',
+                    'attributes' => ['name'],
+                    /*'relationships' => [
+                        'recipes' => []
+                    ]*/
+                ],
+            ],
+        ]);
+});
+
+test('tag show', function () {
+    Sanctum::actingAs(User::factory()->create());
+
+    $tag = Tag::factory()->create();
+
+    $response = $this->get("/api/tags/$tag->id");
+
+    $response->assertStatus(Response::HTTP_OK)
+        ->assertJsonStructure([
+            'data' => [
+                'id',
+                'type',
+                'attributes' => ['name'],
+                'relationships' => [
+                    'recipes' => []
+                ]
+            ],
+        ]);
+});
